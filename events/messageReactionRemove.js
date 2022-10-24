38c17a65ee7c264743e0c2bf0a824c1b63425efa
Eval <zcommon@streamlounge.io>
2022-10-23 19:31:58 -0700

    Fixed reaction roles

--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -12,13 +12,14 @@ module.exports = {
 };
 
 const removeRole = async (reaction, user) => {
+    // Emoji id: Role id
     const reactionRoles = {
         '🔔': '961183420980011038',
-        '<:vrcAlert:456567567742402560>': '961183618074574878',
-        '<:OrcaPraise:754927593035399218>': '961184096728535070',
-        '<:deathroast_gun:833939858493931550>': '961184215133732864',
+        '456567567742402560': '961183618074574878',
+        '754927593035399218': '961184096728535070',
+        '833939858493931550': '961184215133732864',
         '<:Apex_Upvote:543577758689918976>': '961184267243778048',
-        '<:game_arcade:904855079255965808>': '961184413981483038',
+        '904855079255965808': '961184413981483038',
         '📖': '961184500472238090',
         '🎨': '961184649957228554',
     };
@@ -34,14 +35,15 @@ const removeRole = async (reaction, user) => {
 
     if (reaction.message.id !== '961176028720562197') return;
 
-    const guild = client.guilds.cache.get('856279852406931507');
-    let member, role;
-    await guild.members.fetch().then(fetchedMembers => {
-        member = fetchedMembers.filter(guildMember => guildMember.id === user.id);
-    });
-    await guild.roles.fetch().then(fetchedRoles => {
-        role = fetchedRoles.filter(guildRole => guildRole.id === reactionRoles[reaction.emoji.name]);
-    });
+    const guild = await client.guilds.cache.get('856279852406931507');
+    const member = await guild.members.fetch({ user: user.id });
+
+    let role;
+    if (reaction.emoji.id) {
+        role = await guild.roles.fetch(reactionRoles[reaction.emoji.id]);
+    } else {
+        role = await guild.roles.fetch(reactionRoles[reaction.emoji.name]);
+    }
 
     member.roles.remove(role);
 };
